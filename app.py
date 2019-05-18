8482ae4844812ec3f81ee65e98d5cb80a600116c
mrbarge <matt.bargenquast@gmail.com>
2019-05-18 15:16:54 +1000

    hello

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 #!/bin/env python
 import falcon
+import falcon_jsonify
 from wsgiref import simple_server
 
 
-class ThingsResource(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = ('\nTwo things awe me most, the starry sky '
-                     'above me and the moral law within me.\n'
-                     '\n'
-                     '    ~ Immanuel Kant\n\n')
+class HelloResource(object):
+	def on_post(self, req, resp):
+		try:
+			name = req.get_json('name')
+			msg = "Hello " + name
+			resp.json = {"message": msg }
+			resp.status = falcon.HTTP_200
+		except Exception:
+			raise falcon.HTTPError(falcon.HTTP_501)
+		
 
-# falcon.API instances are callable WSGI apps
-app = application = falcon.API()
+app = application = falcon.API(middleware=[
+	falcon_jsonify.Middleware(help_messages=True),
+])
 
-# Resources are represented by long-lived class instances
-things = ThingsResource()
+hello = HelloResource()
 
-# things will handle all requests to the '/things' URL path
-app.add_route('/things', things)
+app.add_route('/hello', hello)
 
 if __name__ == '__main__':
 	http = simple_server.make_server('0.0.0.0', 8080, app)
